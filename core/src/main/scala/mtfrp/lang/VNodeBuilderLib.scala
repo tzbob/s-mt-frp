e59f92fb60f57a5dafb76e36afee1a7610e466d2
Bob R <bob.reynders@cs.kuleuven.be>
2014-10-02 15:56:54 +0200

    define Virtual DOM API

---
+++ core/src/main/scala/mtfrp/lang/VNodeBuilderLib.scala
@@ -0,0 +1,105 @@
+package mtfrp.lang
+
+import scala.js.language.JSMaps
+import scala.js.language.dom.EventOps
+import scala.js.language.JS
+
+trait VNodeBuilderLib extends ClientFRPLib with EventOps with JSMaps with JS {
+  sealed abstract class Value[T]
+  case class RepConst[T](node: Rep[T]) extends Value[T]
+  case class RepList[T](node: Rep[List[T]]) extends Value[T]
+  implicit def repNode[T](node: Rep[T]): Value[T] = RepConst(node)
+  implicit def repNodes[T](nodes: Rep[List[T]]): Value[T] = RepList(nodes)
+
+  implicit def repStrToNode(s: Rep[String]): Value[VNode] = mkText(s)
+  implicit def strToVal(s: String)(implicit ev: String => Rep[String]): Value[VNode] =
+    repStrToNode(s)
+
+  implicit class AttrString(k: String) {
+    def :=(v: Rep[String]): Rep[Attribute] = make_tuple2((k, v))
+    def :=(v: String): Rep[Attribute] = make_tuple2((k, v))
+  }
+
+  implicit class RepAttrString(k: Rep[String]) {
+    def :=(v: Rep[String]): Rep[Attribute] = make_tuple2((k, v))
+    def :=(v: String): Rep[Attribute] = make_tuple2((k, v))
+  }
+
+  sealed abstract class VNode
+
+  type Attribute = (String, String)
+
+  def defaultProperties() = JSMap[String, String]()
+  type Properties = Rep[Map[String, String]]
+
+  val defaultHandlers: Handlers = collection.immutable.List.empty
+  type Handlers = List[Handler]
+
+  trait Handler {
+    val eventDef: EventDef
+    val m: Manifest[eventDef.Type]
+    val eventHandler: Rep[eventDef.Type] => Rep[Unit]
+  }
+
+  object Handler {
+    def apply(evtDef: EventDef)(evtHandler: Rep[evtDef.Type] => Rep[Unit])(implicit ma: Manifest[evtDef.Type]) =
+      new Handler {
+        // help the typechecker with a singleton type
+        val eventDef: evtDef.type = evtDef
+        val m = ma
+        val eventHandler = evtHandler
+      }
+  }
+
+  val defaultChildren = List()
+  type Children = Rep[List[VNode]]
+
+  def mkText(str: Rep[String]): Rep[VNode]
+  def mkNode(
+    tagName: Rep[String],
+    properties: Properties = defaultProperties(),
+    handlers: Handlers = defaultHandlers,
+    children: Children = defaultChildren): Rep[VNode]
+
+  private def handleEvent(ev: EventDef)(implicit m: Manifest[ev.Type]): (ClientEvent[ev.Type], Handler) = {
+    val evt = FRP.eventSource[ev.Type](FRP.global)
+    (ClientEvent(evt, ReplicationCore()), Handler(ev)(evt.fire))
+  }
+
+  class EventTargetBuilder(val tagName: Rep[String]) {
+    def apply: VNodeBuilder = new VNodeBuilder(tagName, defaultHandlers)
+    def apply(ev1: EventDef)(implicit m: Manifest[ev1.Type]): (VNodeBuilder, ClientEvent[ev1.Type]) = {
+      val (evt, handler) = handleEvent(ev1)
+      val handlers = handler +: Nil
+      (new VNodeBuilder(tagName, handlers), evt)
+    }
+
+    // ugly boilerplate...
+    def apply(ev1: EventDef, ev2: EventDef)(implicit m1: Manifest[ev1.Type], m2: Manifest[ev2.Type]): (VNodeBuilder, ClientEvent[ev1.Type], ClientEvent[ev2.Type]) = {
+      val (evt1, handler1) = handleEvent(ev1)
+      val (evt2, handler2) = handleEvent(ev2)
+
+      val handlers = handler2 +: handler1 +: Nil
+      (new VNodeBuilder(tagName, handlers), evt1, evt2)
+    }
+    // ... 3,4,5..
+  }
+
+  private def vToRepList[T: Manifest](nvs: Seq[Value[T]]): Rep[List[T]] = {
+    val vnodes = nvs.collect { case RepConst(n) => n }
+    val vnodeLists = list_new(vnodes) +: nvs.collect { case RepList(ns) => ns }
+    vnodeLists.foldLeft(List[T]())(_ ++ _)
+  }
+
+  class VNodeBuilder(tagName: Rep[String], handlers: Handlers) {
+    def apply(): Rep[VNode] = mkNode(tagName, handlers = handlers)
+    def apply(children: Value[VNode]*): Rep[VNode] =
+      mkNode(tagName, children = vToRepList(children))
+    def apply(attrs: Value[Attribute]*)(children: Value[VNode]*): Rep[VNode] = {
+      val jsAttrs = vToRepList(attrs)
+      val props = defaultProperties()
+      jsAttrs.foreach { tuple => props.update(tuple._1, tuple._2) }
+      mkNode(tagName, properties = props, children = vToRepList(children))
+    }
+  }
+}
---
+++ core/src/main/scala/mtfrp/lang/VNodeGroupLib.scala
@@ -0,0 +1,73 @@
+package mtfrp.lang
+
+import scala.js.language.JS
+import scala.js.language.dom.EventOps
+import scala.js.language.JSMaps
+import scala.virtualization.lms.common.TupleOps
+
+trait VNodeGroupLib extends VNodeBuilderLib {
+  implicit class GroupEventTargetBuilder(b: EventTargetBuilder) {
+    def group: GroupBuilder = new GroupBuilder(b.tagName)
+  }
+
+  trait GroupHandler[A] {
+    implicit val m1: Manifest[A]
+    implicit val m2: Manifest[eventDef.Type]
+    val eventDef: EventDef
+    val eventHandler: Rep[(A, eventDef.Type)] => Rep[Unit]
+
+    def toHandler(const: Rep[A]): Handler = {
+      val constHandler = (e: Rep[eventDef.Type]) =>
+        eventHandler(make_tuple2((const, e)))
+      Handler(eventDef)(constHandler)
+    }
+  }
+
+  object GroupHandler {
+    def apply[A: Manifest](evtDef: EventDef)(evtHandler: Rep[(A, evtDef.Type)] => Rep[Unit])(implicit m: Manifest[evtDef.Type]) =
+      new GroupHandler[A] {
+        val m1: Manifest[A] = implicitly[Manifest[A]]
+        val m2: Manifest[eventDef.Type] = m
+        // help the typechecker with a singleton type
+        val eventDef: evtDef.type = evtDef
+        val eventHandler = evtHandler
+      }
+  }
+
+  type GroupHandlers[Id] = List[GroupHandler[Id]]
+
+  private def handleGroupEvent[A: Manifest](ev: EventDef)(implicit m: Manifest[ev.Type]): (ClientEvent[(A, ev.Type)], GroupHandler[A]) = {
+    val evt = FRP.eventSource[(A, ev.Type)](FRP.global)
+    (ClientEvent(evt, ReplicationCore()), GroupHandler(ev)(evt.fire))
+  }
+
+  class GroupBuilder(tagName: Rep[String]) {
+    def apply[Id: Manifest](ev1: EventDef)(implicit m: Manifest[ev1.Type]): (TemplatedVNodeBuilder[Id], ClientEvent[(Id, ev1.Type)]) = {
+      val (evt, handler) = handleGroupEvent[Id](ev1)
+      (new TemplatedVNodeBuilder(tagName, handler +: Nil), evt)
+    }
+
+    // ugly boilerplate...
+    def apply[Id: Manifest](ev1: EventDef, ev2: EventDef)(implicit m1: Manifest[ev1.Type], m2: Manifest[ev2.Type]): (TemplatedVNodeBuilder[Id], ClientEvent[(Id, ev1.Type)], ClientEvent[(Id, ev2.Type)]) = {
+      val (evt1, handler1) = handleGroupEvent[Id](ev1)
+      val (evt2, handler2) = handleGroupEvent[Id](ev2)
+
+      val handlers = handler2 +: handler1 +: Nil
+      (new TemplatedVNodeBuilder(tagName, handlers), evt1, evt2)
+    }
+    // ... 3,4,5..
+  }
+
+  class TemplatedVNodeBuilder[Id: Manifest](tagName: Rep[String], groupHandlers: GroupHandlers[Id]) {
+    private def toVNodeBuilder(id: Rep[Id]): VNodeBuilder = {
+      val handlers = groupHandlers.map(_.toHandler(id))
+      new VNodeBuilder(tagName, handlers)
+    }
+    def apply(id: Rep[Id]): Rep[VNode] = toVNodeBuilder(id)()
+    def apply(id: Rep[Id], children: Value[VNode]*): Rep[VNode] =
+      toVNodeBuilder(id)(children: _*)
+    def apply(id: Rep[Id], attrs: Value[Attribute]*)(children: Value[VNode]*): Rep[VNode] =
+      toVNodeBuilder(id)(attrs: _*)(children: _*)
+  }
+
+}
